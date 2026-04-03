897bd5318ed2a80fde30d14556f7c832a887dacd
RTBlanco <ronnytoribio1@hotmail.com>
2026-04-02 23:31:34 -0400

    added todo comments

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,10 @@ const form = document.getElementById("wire-form") as HTMLFormElement
 form?.addEventListener('submit', (e) => {
   e.preventDefault();
   const formData = new FormData(form);
+  // go through the object and convert it to numbers
   const formObject = { ...Object.fromEntries(formData.entries()) };
 
+  
   const amps = parseFloat(formObject.amps as string);
   const length = parseFloat(formObject.length as string) * 0.3048;
   const volts = parseFloat(formObject.volts as string);
@@ -20,8 +22,6 @@ form?.addEventListener('submit', (e) => {
   const area_mm2 = area_m2 * 1_000_000;
   const awg = getAWG(area_mm2);
 
-  checkIfAllFilled(formData)
-
   const results = document.getElementById("result-section")
 
   if (results?.classList.contains('hidden')) {
@@ -29,20 +29,16 @@ form?.addEventListener('submit', (e) => {
   }
 
   displayRecommendation(awg)
-  document.querySelector('p')!.textContent = `Recommended AWG: ${awg}`;
-  console.log(`Area: ${area_mm2.toFixed(4)} mm²`);
-  console.log(`Recommended AWG: ${awg}`);
+  // document.querySelector('p')!.textContent = `Recommended AWG: ${awg}`;
+  // console.log(`Area: ${area_mm2.toFixed(4)} mm²`);
+  // console.log(`Recommended AWG: ${awg}`);
 });
 
-// make it show whem entered
-function checkIfAllFilled(form: FormData) {
-  const formObject = { ...Object.fromEntries(form.entries()) }
 
-  console.log(formObject)
-}
-
+// make accept the formated object
 function displayRecommendation(awg: number) {
   (document.getElementById('awg-number') as HTMLElement).innerText = `${awg}`
+  // (document.getElementById('res-vdrop') as HTMLElement).innerText = 
 }
 
 // design it like its ment for steves. shop 
